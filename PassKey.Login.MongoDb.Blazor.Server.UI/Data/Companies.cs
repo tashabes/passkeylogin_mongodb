3ddce5511a6168f05ce035b3938a4472818cab85
tashabes <natasha_abel@hotmail.co.uk>
2023-08-03 12:03:51 +0100

    Added in data models

---
+++ PassKey.Login.MongoDb.Blazor.Server.UI/Data/Companies.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace PassKey.Login.MongoDb.Blazor.Server.UI.Data
+{
+    public class Companies
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string FileName { get; set; }
+
+        public byte Image_Data { get; set; }
+        public string Color { get; set; }
+
+    }
+}
---
+++ PassKey.Login.MongoDb.Blazor.Server.UI/Data/Users.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace PassKey.Login.MongoDb.Blazor.Server.UI.Data
+{
+ 
+    public class Users
+    {
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; } = MongoDB.Bson.ObjectId.GenerateNewId().ToString();
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string SystemDetail { get; set; }
+
+        public string MachineName2 { get; set; }
+
+        public string OperatingSystem { get; set; }
+
+        public string System { get; set; }
+
+        public string UniqueIdentifier { get; set; }
+
+        public string IPAddress { get; set; }
+
+        public string CompanyName { get; set; }
+
+
+        public string CompanyId { get; set; }
+
+    }
+}
