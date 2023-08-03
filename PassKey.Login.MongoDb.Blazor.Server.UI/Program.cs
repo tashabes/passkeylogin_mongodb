11f05a735dde5ccf6bb9b1e30d2b102041677c19
tashabes <natasha_abel@hotmail.co.uk>
2023-08-03 11:55:52 +0100

    Deleted all unused files

--- PassKey.Login.MongoDb.Blazor.Server.UI/Program.cs
+++ PassKey.Login.MongoDb.Blazor.Server.UI/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using PassKey.Login.MongoDb.Blazor.Server.UI.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 
 var app = builder.Build();
 
